5b87992e246995178776c5491c6d88ae8a1b3f0d
Alex <alexkrieg@gmx.ch>
2024-03-21 22:21:36 +0100

    -

--- core/src/components/drawable/PointPainter.cpp
+++ core/src/components/drawable/PointPainter.cpp
@@ -29,14 +29,12 @@ namespace QSFML
             m_defaultRadius = radius;
             for (auto& point : m_points)
             {
-                //updateShape(point, radius, point.position);
                 point.radius = radius;
             }
         }
         void PointPainter::setRadius(size_t index, float radius)
         {
             m_points[index].radius = radius;
-            //updateShape(m_points[index], radius, m_points[index].position);
         }
         float PointPainter::getRadius() const
         {
@@ -45,7 +43,6 @@ namespace QSFML
         float PointPainter::getRadius(size_t index) const
         {
             return m_points[index].radius;
-            //return m_points[index].vertecies[0].position.x - m_points[index].position.x;
         }
 
         void PointPainter::setVerteciesCount(size_t count)
@@ -62,13 +59,11 @@ namespace QSFML
             m_defaultColor = color;
             for (auto& point : m_points)
             {
-                //updateColor(point, color);
 				point.color = color;
 			}
         }
         void PointPainter::setColor(size_t index, const sf::Color& color)
         {
-            //updateColor(m_points[index], color);
 		    m_points[index].color = color;
         }
         const sf::Color& PointPainter::getColor() const
@@ -77,22 +72,16 @@ namespace QSFML
         }
         const sf::Color& PointPainter::getColor(size_t index) const
         {
-            //return m_points[index].vertecies[0].color;
             return m_points[index].color;
         }
 
         void PointPainter::setPoint(const sf::Vector2f& pos)
         {
-            //m_points = { {pos, m_defaultColor, m_defaultRadius} };
             m_points = { {pos, m_defaultColor, m_defaultRadius}};
-            //m_points.back().buffer.create(m_verteciesCount);
-           // updateShape(m_points[0], m_defaultRadius, pos);
         }
         void PointPainter::setPoint(const sf::Vector2f& pos, float radius, const sf::Color& color)
         {
             m_points = { {pos, color, radius} };
-           // m_points.back().buffer.create(m_verteciesCount);
-            //updateAll(m_points[0], radius, pos, color);
         }
         void PointPainter::setPoints(const std::vector<sf::Vector2f>& points)
         {
@@ -100,24 +89,16 @@ namespace QSFML
             m_points.reserve(points.size());
             for (auto& pos : points)
             {
-				//m_points.push_back({ pos, m_defaultColor, m_defaultRadius });
 				m_points.push_back({ pos, m_defaultColor, m_defaultRadius });
-                //updateAll(m_points.back(), m_defaultRadius, pos, m_defaultColor);
-                //m_points.back().buffer.create(m_verteciesCount);
 			}
         }
         void PointPainter::addPoint(const sf::Vector2f& pos)
         {
-            //m_points.push_back({pos, m_defaultColor, m_defaultRadius});
             m_points.push_back({ pos, m_defaultColor, m_defaultRadius});
-            //m_points.back().buffer.create(m_verteciesCount);
         }
         void PointPainter::addPoint(const sf::Vector2f& pos, float radius, const sf::Color& color)
         {
-            //m_points.push_back({ pos, color, radius });
             m_points.push_back({ pos, m_defaultColor, m_defaultRadius});
-            //m_points.back().buffer.create(m_verteciesCount);
-           // updateAll(m_points.back(), radius, pos, color);
         }
         PointPainter::PointData& PointPainter::getPoint(size_t index)
         {
@@ -131,23 +112,5 @@ namespace QSFML
         {
             m_points.clear();
         }
-
-        /*
-        void PointPainter::draw(sf::RenderTarget& target,
-            sf::RenderStates states) const
-        {
-            sf::CircleShape circle(1,m_verteciesCount);
-            circle.setOutlineThickness(0);
-            for (auto& point : m_points)
-            {
-                circle.setRadius(point.radius);
-                circle.setOrigin(point.radius, point.radius);
-                circle.setPosition(point.position);
-                circle.setFillColor(point.color);
-                
-                target.draw(circle, states);
-            }
-        }
-        */
     }
 }
