f4ee13188ea69054cb1adf25c7327d8c575bcecf
Alex <alexkrieg@gmx.ch>
2024-09-19 17:45:19 +0200

    -

--- core/inc/QSFML_EditorWidget_debug.h
+++ core/inc/QSFML_EditorWidget_debug.h
@@ -1,8 +1,12 @@
 #pragma once
 #include "QSFML_EditorWidget_global.h"
 
+/// USER_SECTION_START 14
+
+/// USER_SECTION_END
+
 // The Logger library is automaticly included if the logger dependency .cmake file is available
-#if defined(LOGGER) and not defined(LOGGER_LIB)
+#if defined(LOGGER)
 	#include "Logger.h"
 #endif
 
@@ -107,7 +111,7 @@ namespace QSFML
 /// USER_SECTION_END
 
 
-#if defined(LOGGER) and not defined(LOGGER_LIB)
+#if defined(LOGGER)
 	class QSFML_EDITOR_WIDGET_EXPORT Logger 
 	{
 		/// USER_SECTION_START 8
@@ -161,6 +165,9 @@ namespace QSFML
 
 /// USER_SECTION_END
 #endif
+/// USER_SECTION_START 13
+
+/// USER_SECTION_END
 }
 
 
---
+++ documentation/Canvas.md
@@ -0,0 +1,8 @@
+# Canvas
+
+## Hirarchy
+<img src="CanvasObjectHirarchy.png" alt="Object hirarchy" width="200"/><br>
+
+
+## Update loop
+<img src="UpdateLoopSingle.png" alt="Object hirarchy" width="200"/><br>
