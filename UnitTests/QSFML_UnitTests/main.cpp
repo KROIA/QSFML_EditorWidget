4d27e9f9909272894f66c032c0e94d92ed0c508f
Alex <alexkrieg@gmx.ch>
2024-10-10 00:19:39 +0200

    -

--- UnitTests/QSFML_UnitTests/main.cpp
+++ UnitTests/QSFML_UnitTests/main.cpp
@@ -29,44 +29,6 @@ int main(int argc, char* argv[])
 #endif
 
 
-	QSFML::SceneSettings settings;
-
-	// Background color of parts on which nothing was painted
-	settings.colors.defaultBackground = sf::Color(80, 80, 80);
-
-	// sf::ContextSettings for the render window
-	settings.contextSettings.antialiasingLevel = 8;
-
-	// QLayout margin for when the Scene has a QWidget parent
-	settings.layout.margin.left = 6;
-	settings.layout.margin.top = 6;
-	settings.layout.margin.right = 6;
-	settings.layout.margin.bottom = 6;
-
-	settings.layout.autoAjustSize = true; // If set to true, the Scene will automatically resizes based on its parent widget.
-	settings.layout.fixedSize = sf::Vector2u(1000, 700); // If autoAjustSize is false, this size will be applyed to the Scene widget
-
-	settings.timing.frameTime = 0.01;
-	settings.timing.physicsDeltaTScale = 0.1;
-	settings.timing.physicsFixedDeltaT = 0.01;
-
-	// Enable or disable the different loops
-	settings.updateControlls.enableEventLoop = true;
-	settings.updateControlls.enablePaintLoop = true;
-	settings.updateControlls.enableUpdateLoop = true;
-
-	// Enable or disable multithreading for the update loop
-	settings.updateControlls.enableMultithreading = false;
-
-	QWidget widget;
-	QSFML::Scene* scene = new QSFML::Scene(&widget, settings);
-	widget.show();
-	auto defaultEditor = new QSFML::Objects::DefaultEditor();
-	//defaultEditor->setColors(sf::Color(200, 200, 200), sf::Color(0, 0, 0));
-	scene->addObject(defaultEditor);
-	scene->start();
-	return app.exec();
-
 	Log::UI::NativeConsoleView console;
 	console.show();
 	QSFML::LibraryInfo::printInfo();
