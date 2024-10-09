3a8a07d2979e62613aaadec320927111c092cf81
Alex Krieg <alexkrieg@gmx.ch>
2024-10-09 13:34:21 +0200

    + DigSig

---
+++ Examples/DigSig/src/main.cpp
@@ -0,0 +1,20 @@
+#include "ExampleScene.h"
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+#if QT_VERSION >= QT_VERSION_CHECK(5, 14, 0)
+    // Qt Compatibility for high DPI displays
+    QGuiApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+    QGuiApplication::setAttribute(Qt::AA_UseHighDpiPixmaps);
+    QGuiApplication::setHighDpiScaleFactorRoundingPolicy(Qt::HighDpiScaleFactorRoundingPolicy::PassThrough);
+#endif
+    QApplication a(argc, argv);
+    ExampleScene* w = new ExampleScene();
+    w->show();
+
+    int ret = a.exec();
+    delete w;
+    qDebug() << "Objects that are not deleted: " << QSFML::Internal::LifetimeChecker::getAliveCount() << " of total: " << QSFML::Internal::LifetimeChecker::getTotalCount();
+    return ret;
+}
