1d2082da5e003e255c1c9e1bb6a1127dcf1f9036
Alex <alexkrieg@gmx.ch>
2024-10-21 23:20:20 +0200

    -

--- core/src/utilities/PerlinNoise.cpp
+++ core/src/utilities/PerlinNoise.cpp
@@ -21,16 +21,16 @@ namespace QSFML
 		PerlinNoise::~PerlinNoise() 
 		{}
 
-		float PerlinNoise::noise(float x, float y, int octaves, const sf::Vector2u& gridsize) const
+		double PerlinNoise::noise(double x, double y, int octaves, const sf::Vector2u& gridsize) const
 		{
 			//QSFMLP_GENERAL_FUNCTION(QSFML_COLOR_STAGE_1);
-			float value = 0.0f;
-			float amplitude = 1.0f;
-			float frequency = 1.0f;
-			float maxValue = 0.0f;  // Used for normalizing result to 0.0 - 1.0
+			double value = 0.0f;
+			double amplitude = 1.0f;
+			double frequency = 1.0f;
+			double maxValue = 0.0f;  // Used for normalizing result to 0.0 - 1.0
 
-			float gridSize_x = 1.f/gridsize.x;
-			float gridSize_y = 1.f/gridsize.y;
+			double gridSize_x = 1.f/gridsize.x;
+			double gridSize_y = 1.f/gridsize.y;
 
 			for (int i = 0; i < octaves; i++) {
 				value += perlin(x * (frequency * gridSize_x), y * (frequency * gridSize_y)) * amplitude;
@@ -63,7 +63,7 @@ namespace QSFML
 			a *= 2048419325;
 			a ^= m_seed; // Incorporate the seed into the hash calculation
 
-			float random = (float)a * ((float)M_PI / (float)(~(~0u >> 1))); // in [0, 2*Pi]
+			double random = (double)a * ((double)M_PI / (double)(~(~0u >> 1))); // in [0, 2*Pi]
 
 			// Create the vector from the angle
 			sf::Vector2f v;
@@ -74,27 +74,27 @@ namespace QSFML
 		}
 
 		// Computes the dot product of the distance and gradient vectors.
-		float PerlinNoise::dotGridGradient(int ix, int iy, float x, float y) const 
+		double PerlinNoise::dotGridGradient(int ix, int iy, double x, double y) const 
 		{
 			// Get gradient from integer coordinates
 			sf::Vector2f gradient = randomGradient(ix, iy);
 
 			// Compute the distance vector
-			float dx = x - (float)ix;
-			float dy = y - (float)iy;
+			double dx = x - (double)ix;
+			double dy = y - (double)iy;
 
 			// Compute the dot-product
 			return (dx * gradient.x + dy * gradient.y);
 		}
 
-		float PerlinNoise::interpolate(float a0, float a1, float w) const
+		double PerlinNoise::interpolate(double a0, double a1, double w) const
 		{
 			return (a1 - a0) * (3.0f - w * 2.0f) * w * w + a0;
 		}
 
 
 		// Sample Perlin noise at coordinates x, y
-		float PerlinNoise::perlin(float x, float y) const
+		double PerlinNoise::perlin(double x, double y) const
 		{
 			//QSFMLP_GENERAL_FUNCTION(QSFML_COLOR_STAGE_2);
 			// Determine grid cell corner coordinates
@@ -104,21 +104,21 @@ namespace QSFML
 			int y1 = y0 + 1;
 
 			// Compute Interpolation weights
-			float sx = x - (float)x0;
-			float sy = y - (float)y0;
+			double sx = x - (double)x0;
+			double sy = y - (double)y0;
 
 			// Compute and interpolate top two corners
-			float n0 = dotGridGradient(x0, y0, x, y);
-			float n1 = dotGridGradient(x1, y0, x, y);
-			float ix0 = interpolate(n0, n1, sx);
+			double n0 = dotGridGradient(x0, y0, x, y);
+			double n1 = dotGridGradient(x1, y0, x, y);
+			double ix0 = interpolate(n0, n1, sx);
 
 			// Compute and interpolate bottom two corners
 			n0 = dotGridGradient(x0, y1, x, y);
 			n1 = dotGridGradient(x1, y1, x, y);
-			float ix1 = interpolate(n0, n1, sx);
+			double ix1 = interpolate(n0, n1, sx);
 
 			// Final step: interpolate between the two previously interpolated values, now in y
-			float value = interpolate(ix0, ix1, sy);
+			double value = interpolate(ix0, ix1, sy);
 
 
 			return value;
