032223c7399bee20021738b06f9766a8bc453cc2
Alex Krieg <alexkrieg@gmx.ch>
2024-10-26 11:43:16 +0200

    ~ Update Library Template 1.4.0

--- dependencies/SFML.cmake
+++ dependencies/SFML.cmake
@@ -13,6 +13,7 @@ function(dep LIBRARY_MACRO_NAME SHARED_LIB STATIC_LIB STATIC_PROFILE_LIB)
         GIT_REPOSITORY ${GIT_REPO}
         GIT_TAG        ${GIT_TAG}
     )
+
     # Specific SFML settings
     # SFML Static Lib
     set(SFML_MISC_INSTALL_PREFIX "${INSTALL_LIB_PATH}/build/misc") 
@@ -22,8 +23,18 @@ function(dep LIBRARY_MACRO_NAME SHARED_LIB STATIC_LIB STATIC_PROFILE_LIB)
 
 
     set(BUILD_SHARED_LIBS OFF CACHE BOOL "Build SFML as static library.")
-    message("Downloading dependency: ${LIB_NAME} from: ${GIT_REPO} tag: ${GIT_TAG}")
-    FetchContent_MakeAvailable(${LIB_NAME})
+
+    # Check if the library has already been populated
+    FetchContent_GetProperties(${LIB_NAME})
+    if(NOT ${LIB_NAME}_ALREADY_POPULATED)
+        message("Downloading dependency: ${LIB_NAME} from: ${GIT_REPO} tag: ${GIT_TAG}")
+        FetchContent_MakeAvailable(${LIB_NAME})
+        # Set a persistent cache variable to mark the library as populated
+        set(${LIB_NAME}_ALREADY_POPULATED TRUE CACHE INTERNAL "Mark ${LIB_NAME} as populated")
+    else()
+        # Re-run MyLibrary's CMakeLists.txt to set up include dirs, libraries, etc.
+        add_subdirectory("${${LIB_NAME}_SOURCE_DIR}" "${${LIB_NAME}_BINARY_DIR}" EXCLUDE_FROM_ALL)
+    endif()
 
     target_compile_definitions(sfml-graphics	PRIVATE		SFML_STATIC)
     target_compile_definitions(sfml-audio		PRIVATE		SFML_STATIC)
